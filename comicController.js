64f2b407b5a31816c13ce93660b160480c000373
Adam Davies <adamd94@hotmail.com>
2016-06-03 16:47:26 +0100

    navigation displays correctly at border comics
    Implemented css + angular code to display correct navigation when at the
    beginning and end comics

--- comicController.js
+++ comicController.js
@@ -7,35 +7,49 @@ app.controller('comicController', function($scope){
 		"comics/img3.png",
 		"comics/img4.png"
 	];
-	
+
 	$scope.currentComicIndex = $scope.comics.length - 1;				//set current comic index to the last comic
 	$scope.comicURL = $scope.comics[$scope.currentComicIndex];			//display current comic
 	
-	
-	$scope.debug = Math.floor(Math.random() * $scope.comics.length);	//variable display for debugging purposes
+		
+	//$scope.debug = Math.floor(Math.random() * $scope.comics.length);	//variable display for debugging purposes
 	
 	$scope.switchComic = function(cci){
 		if(cci < $scope.comics.length && cci >= 0){
 			$scope.currentComicIndex = cci;
 			$scope.comicURL = $scope.comics[cci];
+			$scope.prevStyle = {"display" : "inline"};
+			$scope.nextStyle = {"display" : "inline"};
+			
+			if(cci == 0){
+				$scope.prevStyle = {"display" : "none"};
+				return;
+			}
+			if(cci == $scope.comics.length - 1){
+				$scope.nextStyle = {"display" : "none"};
+				return;
+			}
 		}
 	}
 	
-	$scope.next = function (){
+	$scope.switchComic($scope.comics.length - 1); //required to get correct navigation display on initialization
+	
+	$scope.next =     function (){
 		$scope.switchComic($scope.currentComicIndex + 1);
 	}
 	$scope.previous = function (){
 		$scope.switchComic($scope.currentComicIndex - 1);
 	}
-	$scope.newest = function () {
+	$scope.newest =   function (){
 		$scope.switchComic($scope.comics.length - 1);
 	}
-	$scope.oldest = function () {
+	$scope.oldest =   function (){
 		$scope.switchComic(0);
 	}
-	$scope.random = function () {
-		var randIndex = Math.floor(Math.random() * $scope.comics.length);
-		$scope.switchComic(randIndex);
+	$scope.random =   function (){
+		do {
+			$scope.randIndex = Math.floor(Math.random() * $scope.comics.length);//get random comic index's...
+		} while ($scope.randIndex == $scope.currentComicIndex)					//until the index obtained is different than the current comics index
+		$scope.switchComic($scope.randIndex);
 	}
-
 });
